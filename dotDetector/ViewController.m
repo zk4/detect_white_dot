d7f54581375dea101cc8ddd289ae699b135fc53e
zk <liuzq7@gmail.com>
2020-08-04 03:20:14 +0800

    concise api

--- dotDetector/ViewController.m
+++ dotDetector/ViewController.m
@@ -62,45 +62,7 @@ RGB MakeRGB(int r,int g ,int b){
          }
      }
 }
-- (NSMutableArray*) dectectWhitestDot:(const unsigned char*) array width:(int) width height:(int)height isWhiteThreadhold:(float) whiteThreash  boundingX:(int)bx boundingY:(int)by{
-    float maxavg = 0.f;
-      NSMutableArray * nsa =  [[NSMutableArray alloc]init];
-      CGRect ret = CGRectMake(-1,-1,0,0);
-       for(int y = 0; y< height-by+1; y++){
-           for (int x = 0; x< width-bx+1; x++){
-               float ava = 0 ;
-               int count = 0;
-               for (int iy = 0;iy < by; iy++){
-                   if (y+iy >= height)
-                       break;
-                   for(int ix = 0; ix < bx; ix++){
-                       if (x+ix >= width)
-                           break;
-                       int  point = (x+ix+(y+iy)*width)*4;
-                       float r = array[point]/255.0;
-                       float g = array[point+1]/255.0;
-                       float b = array[point+2]/255.0;
-                       float a =array[point+3]/255.0;
-                       ava +=(r+g+b)/3.0f;
-                       count ++;
-                   }
-               }
-               ava = ava / (count*1.0f);
-               if(ava >= whiteThreash){
-                   if(ava>= maxavg){
-                      maxavg =ava;
-                      ret = CGRectMake(x, y, bx, by);
-                      
-                      NSLog(@"%d,%d, ava %f, probably iswhite",x,y, ava);
-                   }
-
-               }
-           }
-      }
-    [nsa addObject:[NSValue valueWithCGRect:ret]];
-    return nsa;
-}
-- (NSMutableArray *) dectectWhiteDots:(const unsigned char*) array width:(int) width height:(int)height isWhiteThreadhold:(float) whiteThreash  boundingX:(int)bx boundingY:(int)by{
+- (NSMutableArray *) dectectWhiteDots:(const unsigned char*) array width:(int) width height:(int)height isWhiteThreadhold:(float) whiteThreash  boundingX:(int)bx boundingY:(int)by fetchOnlyWhitest:(BOOL)whitest{
     float maxavg = 0.f;
     NSMutableArray * nsa =  [[NSMutableArray alloc]init];
     CGRect ret = CGRectMake(-1,-1,0,0);
@@ -128,14 +90,16 @@ RGB MakeRGB(int r,int g ,int b){
                  if(ava>= maxavg){
                     maxavg =ava;
                     ret = CGRectMake(x, y, bx, by);
-                    [nsa addObject:[NSValue valueWithCGRect:ret]];
+                     if(!whitest)
+                         [nsa addObject:[NSValue valueWithCGRect:ret]];
                     NSLog(@"%d,%d, ava %f, probably iswhite",x,y, ava);
                  }
 
              }
          }
     }
-    
+  if(whitest)
+    [nsa addObject:[NSValue valueWithCGRect:ret]];
   return nsa;
 }
  - (UIImage *)convertImageToGrayScale:(UIImage *)image {
@@ -178,7 +142,7 @@ RGB MakeRGB(int r,int g ,int b){
 //    image = [self convertImageToGrayScale:image];
     const unsigned char* bytes=  [ImageHelper convertUIImageToBitmapRGBA8:image];
 
-    NSMutableArray * rects = [self dectectWhitestDot:bytes width:image.size.width height:image.size.height isWhiteThreadhold:.7f  boundingX:10  boundingY:10];
+    NSMutableArray * rects = [self dectectWhiteDots:bytes width:image.size.width height:image.size.height isWhiteThreadhold:.7f  boundingX:10  boundingY:10 fetchOnlyWhitest:YES];
     
       UIImage * newimg= [ViewController drawRectangleOnImage:image where:rects];
 
